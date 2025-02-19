9515da2bb57ed5758b5e968e9b234ce217b7d1ac
Jakub Pisarek <99591440+sgfn@users.noreply.github.com>
2025-02-19 14:13:09 +0100

    Changes from https://github.com/elixir-webrtc/ex_webrtc/pull/197/files

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
 # ExWebRTC Recorder
 
-TODO WRITEME
+[![Hex.pm](https://img.shields.io/hexpm/v/ex_webrtc_recorder.svg)](https://hex.pm/packages/ex_webrtc_recorder)
+[![API Docs](https://img.shields.io/badge/api-docs-yellow.svg?style=flat)](https://hexdocs.pm/ex_webrtc_recorder)
+
+Records and processes RTP packets sent and received using [ExWebRTC](https://github.com/elixir-webrtc/ex_webrtc).
+
+## Installation
+
+Add `:ex_webrtc_recorder` to your list of dependencies
+
+```elixir
+def deps do
+  [
+    {:ex_webrtc_recorder, "~> 0.1.0"}
+  ]
+end
+```
+
+TODO add necessary steps
+
+ExWebRTC Recorder comes with optional support for uploading the recordings to S3-compatible storage,
+but it must be explicitely turned on by adding the following optional dependencies:
+* `:ex_aws_s3`
+* `:ex_aws`
+* `:sweet_xml`
+* an HTTP client (e.g. `:req`)
+
+```elixir
+def deps do
+  [
+    {:ex_webrtc_recorder, "~> 0.1.0"},
+    {:ex_aws_s3, "~> 2.5"},
+    {:ex_aws, "~> 2.5"},
+    {:sweet_xml, "~> 0.7"},
+    {:req, "~> 0.5"}
+  ]
+end
+```
---
+++ mix.exs
@@ -0,0 +1,72 @@
+defmodule ExWebRTC.Recorder.MixProject do
+  use Mix.Project
+
+  @version "0.1.0"
+  @source_url "https://github.com/elixir-webrtc/ex_webrtc_recorder"
+
+  def project do
+    [
+      app: :ex_webrtc_recorder,
+      version: @version,
+      elixir: "~> 1.15",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      start_permanent: Mix.env() == :prod,
+      description: "Records and processes RTP packets sent and received using ExWebRTC",
+      package: package(),
+      deps: deps(),
+
+      # docs
+      source_url: @source_url,
+
+      # dialyzer
+      dialyzer: [
+        plt_local_path: "_dialyzer",
+        plt_core_path: "_dialyzer"
+      ],
+
+      # code coverage
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "coveralls.json": :test
+      ]
+    ]
+  end
+
+  def application do
+    [
+      mod: {ExWebRTC.Recorder.App, []},
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_env), do: ["lib"]
+
+  def package do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => @source_url}
+    ]
+  end
+
+  defp deps do
+    [
+      {:ex_webrtc, github: "elixir-webrtc/ex_webrtc", branch: "sgfn/extract-recorder"},
+      {:jason, "~> 1.4"},
+      {:ex_aws_s3, "~> 2.5", optional: true},
+      {:ex_aws, "~> 2.5", optional: true},
+      {:sweet_xml, "~> 0.7", optional: true},
+      {:req, "~> 0.5", optional: true},
+
+      # dev/test
+      {:excoveralls, "~> 0.18.0", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.31", only: :dev, runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
+    ]
+  end
+end
